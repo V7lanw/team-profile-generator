d2cc66e6b1c347357b5b059ed0569914fd4cad3c
rbmilkveWin10 <v7lanw@hotmail.com>
2023-03-08 16:57:15 +0800

    changed import and export in ES module way, and tested with createManager function in index.js

--- index.js
+++ index.js
@@ -1,15 +1,79 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
+import Manager from "./lib/Manager.js";
+import Engineer from "./lib/Engineer.js";
+import Intern from "./lib/Intern.js";
+import inquirer from "inquirer";
+import path from "path";
+import fs from "fs";
+import url from "url";
 
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./src/page-template.js");
+import render from "./src/page-template.js";
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+const idList = [];
+const teamMembers = [];
 
+const appMenu = () => {
+    function createManager() {
+        console.log("Please build your team!");
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "managerName",
+                message: "What is the team manager's name?",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "Please enter at least one character, name cannot be empty.";
+                }
+            },
+            {
+                type: "input",
+                name: "managerId",
+                message: "What is the team manager's ID?",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "Please enter at least one character, ID cannot be empty.";
+                }
+            },
+            {
+                type: "input",
+                name: "managerEmail",
+                message: "What is the team manager's email?",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "Please enter at least one character, email cannot be empty.";
+                }
+            },
+            {
+                type: "input",
+                name: "managerOfficeNumber",
+                message: "What is the team manager's office number?",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "Please enter at least one character, office number cannot be empty.";
+                }
+            }
+        ]).then(answers => {
+            const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
+            idList.push(answers.managerId);
+            teamMembers.push(manager);
+        });
+    }
+
+    createManager();
+};
+
+appMenu();
