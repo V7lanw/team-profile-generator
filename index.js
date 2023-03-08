7403e2b267c55d74506474eb5ec49c3025ba6737
rbmilkveWin10 <v7lanw@hotmail.com>
2023-03-08 18:30:30 +0800

    added addEngineer and addIntern function and run tests

--- index.js
+++ index.js
@@ -19,6 +19,98 @@ const idList = [];
 const teamMembers = [];
 
 const appMenu = () => {
+    function addEngineer() {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "engineerName",
+                message: "What is the engineer's name?"
+            },
+            {
+                type: "input",
+                name: "engineerId",
+                message: "What is the engineer's ID?"
+            },
+            {
+                type: "input",
+                name: "engineerEmail",
+                message: "What is the engineer's email?"
+            },
+            {
+                type: "input",
+                name: "engineerGithub",
+                message: "What is the engineer's GitHub?"
+            },
+        ]).then(answers => {
+            const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub);
+            idList.push(answers.engineerId);
+            teamMembers.push(engineer);
+            console.log(engineer);
+            createTeam();
+        });
+    }
+
+    function addIntern() {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "internName",
+                message: "What is the intern's name?"
+            },
+            {
+                type: "input",
+                name: "internId",
+                message: "What is the intern's ID?"
+            },
+            {
+                type: "input",
+                name: "internEmail",
+                message: "What is the intern's email?"
+            },
+            {
+                type: "input",
+                name: "internSchool",
+                message: "What is the intern's School?"
+            },
+        ]).then(answers => {
+            const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool);
+            idList.push(answers.internId);
+            teamMembers.push(intern);
+            console.log(intern);
+            createTeam();
+        });
+    }
+
+    function buildTeam() {
+        if (!fs.existsSync(OUTPUT_DIR)) {
+            fs.mkdirSync(OUTPUT_DIR);
+        }
+        fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
+    }
+
+    function createTeam() {
+        inquirer.prompt([
+            {
+                type: "list",
+                name: "memberChoice",
+                message: "Which type of team member would you like to add?",
+                choices: [
+                    "Engineer",
+                    "Intern",
+                    "I don't want to add any more team members"
+                ]
+            }
+        ]).then(userChoice => {
+            if (userChoice.memberChoice === "Engineer") {
+                addEngineer();
+            } else if (userChoice.memberChoice === "Intern") {
+                addIntern();
+            } else {
+                buildTeam();
+            }
+        });
+    }
+
     function createManager() {
         console.log("Please build your team!");
         inquirer.prompt([
@@ -70,6 +162,8 @@ const appMenu = () => {
             const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
             idList.push(answers.managerId);
             teamMembers.push(manager);
+            console.log(manager);
+            createTeam();
         });
     }
 
